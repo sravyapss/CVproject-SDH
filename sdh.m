c77747ebd946067ab7a3e7a8fe063c4e120c639a
sravyapss <32174708+sravyapss@users.noreply.github.com>
2020-04-22 13:04:50 +0530

    Add files via upload

--- sdh.m
+++ sdh.m
@@ -1,30 +1,45 @@
-function XX = sdh(Xtr_p,Xt_p,X_p,labels,n_low,n_high, C, rows)
+function sdh(Xtr_p,Xt_p,X_p,labels,n_low,n_high, C, rows, kind_of_loss, traingnd, testgnd)
     
     
-Po = zeros(33,1);
-Ro = zeros(33,1);
- 
-for num_bits=n_low:1:n_high 
-    
-    disp(num_bits +" bits done");
+Po = zeros(3,1);
+Ro = zeros(3,1);
+
+disp(Po);
+cnt = 1;
+for num_bits=n_low:32:n_high 
+    disp(num_bits +' bits done');
     
     randn('seed',3);
     Zinit=sign(randn(rows,num_bits));
     maxItr = 5;
+    if strcmp(kind_of_loss, 'hinge') == 1
+        [F, G, H] = train_hinge(X_p,labels,Zinit, [],maxItr);
+    else
+        [F, G, H] = train_l2(X_p,labels,Zinit, [],maxItr);
+    end
     
-    [F, G, H] = train(X_p,labels,Zinit, [],maxItr);
-    Ret = Hamming(F,G,H,Xtr_p,Xt_p) ;   
-    [P, R] = Eval(Ret, C);
-    
+    [Ret,hamy] = Hamming(F,G,H,Xtr_p,Xt_p,X_p);
+    [P, R] = Eval(C, Ret);
+
+    % hamming ranking: MAP
+    [~, HammingRank]=sort(hamy,1);
+    MAP = calcMAP(traingnd,testgnd,HammingRank);
     I = num_bits - n_low + 1;
-    Po(I) = P;
-    Ro(I) = R;
+    disp(P);
+    disp(MAP);
+    Po(cnt) = P;
+    Ro(cnt) = R;
+    MAPo(cnt) = MAP;
+    cnt=cnt+1;
 
 end
 
-No = n_low:n_high;
+No = n_low:32:n_high;
+disp(No);
+disp(Po);
 plot(No,Po);
 plot(No,Ro);
+plot(No,MAPo);
 
 
 end
